4b96012f7f71f703688ac0f335636409de01ab94
Trieu Thanh <tvthanh.cod@gmail.com>
2025-01-08 15:58:19 +0700

    endpoint return predict with values predicted and prob

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from model import predict_image, load_model
+
 
 app = FastAPI()
+
+mymodel = load_model();
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/predict-image")
+def predict_single_image():
+   image_path = 'images/image10.png'
+   results = predict_image(image_path, mymodel)
+   return results
