12c39ac59c1008d78a0d23857c9a485df2a3fd13
Trieu Thanh <tvthanh.cod@gmail.com>
2025-01-09 14:45:56 +0700

    add trained predict model

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql://postgres:06122002@localhost/data_images"
+DATABASE_URL_RENDER = "postgresql://dantri_data_user:4lSj7wD5RZe5y3T2m2SR8cmWyvq2yOko@dpg-ctvnprtds78s73eohh40-a/dantri_data"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
